8380a9c283bf96f0667279be885379d5e8656746
z664755810-maker <z664755810@gmail.com>
2026-07-06 10:52:03 +0800

    打印优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
-import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
 export default defineConfig({
   base: '/resume-ZXY/',
@@ -21,14 +20,5 @@ export default defineConfig({
         ],
       },
     }),
-    traeBadgePlugin({
-      variant: 'dark',
-      position: 'bottom-right',
-      prodOnly: true,
-      clickable: true,
-      clickUrl: 'https://www.trae.ai/solo?showJoin=1',
-      autoTheme: true,
-      autoThemeTarget: '#root'
-    })
   ],
 })
